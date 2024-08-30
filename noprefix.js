109b98b5aa32bf4d21a229d6e8f54662d7d9b42f
Vishu7772 <162317231+Vishu7772@users.noreply.github.com>
2024-08-30 18:26:14 +0530

    Update noprefix.js

--- noprefix.js
+++ noprefix.js
@@ -19,6 +19,55 @@ if (lauthorizedUsers.includes (message.author.id)) {return;
 }
 
 if (largs[0])
+  return message.reply("Please provide a valid subcommand: "add", "remove", "list".");
+I
 
-  }
 }
+
+const subCommand = args[0].toLowerCase();
+
+if (subCommand === 'add')
+
+const user message.mentions.first();
+
+if (luser) {
+
+return message.reply("Please mention a user to add.");
+}
+
+try {
+
+const existingUser = await NoPrefixSchema.findOne({userId: user.id }); if (existingUser)
+
+{
+
+return message.reply("User is already in the no-prefix list.");
+
+}
+
+const newUser = new NoPrefixSchema ((userId: user.id });
+
+await newUser.save();
+
+message.reply("Successfully added ${user.tag) to the no-prefix list."); }
+catch (error)
+  {
+
+console.error(error);
+
+message.reply(An error occured.");
+
+}
+
+} else if (subCommand === 'remove') {
+
+const user message.mentions.first();
+
+if (luser) {
+
+return message.reply("Please mention a user to remove.");
+
+}
+  
+  
+
