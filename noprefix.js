f9e35599bbfeec7f6e44882c7f2a27ddbe1a69a9
Vishu7772 <162317231+Vishu7772@users.noreply.github.com>
2024-08-30 19:29:30 +0530

    Update noprefix.js

--- noprefix.js
+++ noprefix.js
@@ -1,6 +1,6 @@
 const { EmbedBuilder, PermissionsBitField } = require('discord.js');
 
-const NoPrefixSchema = require('../Schemas.js/noPrefixSchema');
+const NoPrefixSchema = require('../noPrefixSchema.js');
 
 const authorizedUsers = ['1187666470990266410', 'ID2', 'ID3'];
 
@@ -65,9 +65,72 @@ const user message.mentions.first();
 
 if (luser) {
 
-return message.reply("Please mention a user to remove.");
+return message.reply("Please mention a user to remove.");}
+
+try {
+
+const removedUser await noPrefixSchema.findOneAndDelete ((userId: user.id));
+
+if (IremovedUser) {
+
+return message.reply("User is not in the no-prefix list.");
+
+I
 
 }
+
+message.reply("Successfully removed $(user.tag) from the no-prefix list.");
+
+} catch (error) {
+
+console.error(error);
+
+message.reply('An error occured while removing the user.');
+
+}
+
+}
+}
+
+} else if (subCommand === 'list') {
+
+try {
+
+const users await noPrefixSchema.find({});
+
+if (users.length === 0) {
+
+return message.reply('The no-prefix list is empty');
+
+}
+
+const userList = users.map(user => <@${user.userId)>').join('\n');
+
+const embed = new EmbedBuilder()
+
+.setTitle('No Prefix Users')
+
+.setDescription(userList)
+
+.setColor('Random')
+
+message.reply({ embeds: [embed] });
+  } catch (error) {
+
+console.error(error);
+
+message.reply('An error occured.');
+
+}
+
+I
+
+} else
+
+message.reply('Invalid subcommand. please user "add", "remove", "list');
+}
+                                       }
+                           }
   
   
 
