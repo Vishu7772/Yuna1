60e6655d69836df16183ef6c509156ddb4006d8f
Vishu7772 <162317231+Vishu7772@users.noreply.github.com>
2024-08-30 17:39:32 +0530

    Update noprefix.js

--- noprefix.js
+++ noprefix.js
@@ -1,1 +1,24 @@
+const { EmbedBuilder, PermissionsBitField } = require('discord.js');
 
+const NoPrefixSchema = require('../Schemas.js/noPrefixSchema');
+
+const authorizedUsers = ['1187666470990266410', 'ID2', 'ID3'];
+
+I
+
+module.exports = {
+
+name: 'noprefix',
+
+description: 'Manage no prefix users',
+
+run:async (client, message, args) => {
+
+if (lauthorizedUsers.includes (message.author.id)) {return;
+
+}
+
+if (largs[0])
+
+  }
+}
