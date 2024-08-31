f1f5a4ae92ea5690a047b9cc6df7fb073f60bd20
Vishu7772 <162317231+Vishu7772@users.noreply.github.com>
2024-08-31 17:55:24 +0530

    Update kick.js

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('discord.js');
 const cmdIcons = require('../../UI/icons/commandicons');
 const lang = require('../../events/loadLanguage');
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 1000;
 
 module.exports = {
     data: new SlashCommandBuilder()
