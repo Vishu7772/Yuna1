70ab0acc93af5321dbcde72a38b64d3ff0221237
Atrone <162317231+Vishu7772@users.noreply.github.com>
2025-04-17 03:39:06 +0530

    Update help.js

--- commands/basic/help.js
+++ commands/basic/help.js
@@ -201,7 +201,7 @@ module.exports = {
                     .setAuthor({
                         name: page.author.name,
                         iconURL: BOT_ICON,
-                        url: "https://discord.gg/xQF9f9yUEM"
+                        url: "https://discord.gg/atrone77"
                     })
                     .setImage(helpBanner)
                     .setFooter({ text: `${FOOTER_TEXT} • Page ${currentPage + 1}/${currentSet.length}` })
@@ -355,7 +355,7 @@ module.exports = {
                 .setAuthor({
                     name: "Command Error",
                     iconURL: cmdIcons.dotIcon,
-                    url: "https://discord.gg/xQF9f9yUEM"
+                    url: "https://discord.gg/obontop"
                 })
                 .setDescription('> ⚠️ This command can only be used as a slash command!\n> Please use `/help` instead.')
                 .setFooter({ text: 'All In One Bot • Error' })
